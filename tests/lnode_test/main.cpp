33575e2fc055e06809f8bfc263b283b3269fdd42
aashis-kishore <aashiskishore@gmail.com>
2021-09-25 01:26:50 +0530

    Add test to check node linking

--- tests/lnode_test/main.cpp
+++ tests/lnode_test/main.cpp
@@ -12,3 +12,18 @@ TEST(LNodeTest, create_new_lnode) {
 
   del_node(lnode);
 }
+
+TEST(LNodeTest, create_two_lnodes_and_link) {
+  std::string first_name = "Aashis";
+  std::string last_name = "Kishore";
+
+  LNode* fname = new_node(&first_name);
+  LNode* lname = new_node(&last_name);
+
+  link_node(fname, lname);
+
+  EXPECT_EQ(next_node(fname), lname);
+
+  del_node(fname);
+  del_node(lname);
+}
