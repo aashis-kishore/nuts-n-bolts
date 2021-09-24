8370f35cbfb80b7df537127541becfe5ccb35679
aashis-kishore <aashiskishore@gmail.com>
2021-09-25 01:32:40 +0530

    Add test to check node linking among multiple nodes

--- tests/lnode_test/main.cpp
+++ tests/lnode_test/main.cpp
@@ -5,7 +5,7 @@ extern "C" {
 }
 
 TEST(LNodeTest, create_new_lnode) {
-  std::string name = "Aashis Kishore";
+  std::string name = "John Doe";
   LNode* lnode = new_node(&name);
 
   EXPECT_NE(lnode, nullptr);
@@ -14,8 +14,8 @@ TEST(LNodeTest, create_new_lnode) {
 }
 
 TEST(LNodeTest, create_two_lnodes_and_link) {
-  std::string first_name = "Aashis";
-  std::string last_name = "Kishore";
+  std::string first_name = "John";
+  std::string last_name = "Doe";
 
   LNode* fname = new_node(&first_name);
   LNode* lname = new_node(&last_name);
@@ -25,5 +25,25 @@ TEST(LNodeTest, create_two_lnodes_and_link) {
   EXPECT_EQ(next_node(fname), lname);
 
   del_node(fname);
+  del_node(lname);
+}
+
+TEST(LNodeTest, create_three_lnodes_and_link) {
+  std::string first_name = "John";
+  std::string middle_name = "Conman";
+  std::string last_name = "Doe";
+
+  LNode* fname = new_node(&first_name);
+  LNode* mname = new_node(&middle_name);
+  LNode* lname = new_node(&last_name);
+
+  link_node(fname, mname);
+  link_node(mname, lname);
+
+  EXPECT_EQ(next_node(fname), mname);
+  EXPECT_EQ(next_node(mname), lname);
+
+  del_node(fname);
+  del_node(mname);
   del_node(lname);
 }
