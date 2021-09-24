818e2d75812fd3ee699d93dca9cbb0c5a45513a1
aashis-kishore <aashiskishore@gmail.com>
2021-09-24 09:20:57 +0530

    Linked List

--- src/main.c
+++ src/main.c
@@ -1,9 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct _Node {
+  int data;
+  struct _Node* next;
+} Node;
+
+Node* head = NULL;
+Node* tail = NULL;
+
+void append(int data) {
+  Node* node = calloc(1, sizeof(Node));
+
+  if (!node) {
+    perror("Failed to allocate memory\n");
+    exit(2);
+  }
+
+  node->data = data;
+  node->next = NULL;
+
+  if (!head) {
+    head = node;
+    tail = node;
+    return;
+  }
+
+  tail->next = node;
+  tail = node;
+}
+
+void append_many(int* data, size_t count) {
+  for (size_t i = 0; i < count; i++) {
+    append(data[i]);
+  }
+}
+
+void print() {
+  Node* node = head;
+
+  while(node) {
+    printf("%d ", node->data);
+    node = node->next;
+  }
+
+  printf("\n");
+}
+
+void clear_list() {
+  Node* node = NULL;
+
+  while(head) {
+    node = head;
+    head = head->next;
+
+    free(node);
+  }
+}
+
+void list() {
+  int data[] = {1, 2, 3, 4, 5};
+
+  append_many(data, sizeof(data) / sizeof(data[0]));
+  print();
+
+  clear_list();
+}
+
 int main(void) {
-  printf("Namasthe World\n");
-  printf("I'm Batman");
+  list();
 
   return EXIT_SUCCESS;
 }
