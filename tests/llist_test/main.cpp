ecf1ff626ff0e973534c9d0cb1d70586a8ef973e
aashis-kishore <aashiskishore@gmail.com>
2021-09-25 10:40:43 +0530

    Add test to check data insertion in llist

--- tests/llist_test/main.cpp
+++ tests/llist_test/main.cpp
@@ -5,9 +5,23 @@ extern "C" {
 }
 
 TEST(LListTest, create_new_llist) {
-  LList *llist = new_list();
+  LList* llist = new_list();
 
   EXPECT_NE(llist, nullptr);
+
+  del_list(llist);
+}
+
+TEST(LListTest, append_data_into_llist) {
+  std::string names[]{"John", "Conman", "Doe"};
+
+  LList* llist = new_list();
+
+  append_data(&names[0]);
+  append_data(&names[1]);
+  append_data(&names[2]);
+
+  EXPECT_EQ(len_list(llist), sizeof(names)/sizeof(names[0]));
 
   del_list(llist);
 }
