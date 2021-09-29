2fff24ceacce0de300f1514fc9486bb4f8c823f3
aashis-kishore <aashiskishore@gmail.com>
2021-09-29 13:46:57 +0530

    Define stack creation

--- src/slstack/slstack.c
+++ src/slstack/slstack.c
@@ -1,14 +1,39 @@
 #include "slstack.h"
 
+#include <stdlib.h>
+
 #include "sllist.h"
 
 typedef struct _SLStack {
   SLList* list;
 } SLStack;
 
-SLStack* sls_new() { return NULL; }
+SLStack* sls_new() {
+  SLStack* stack = calloc(1, sizeof(SLStack));
+
+  if (!stack) {
+    perror("Failed to create stack\n");
+    return stack;
+  }
+
+  stack->list = sll_new();
+
+  if (!stack->list) {
+    free(stack);
+    stack = NULL;
+    return stack;
+  }
+
+  return stack;
+}
 
 void sls_del(SLStack* stack) {
   if (stack) {
+    if (stack->list) {
+      sll_del(stack->list);
+    }
+
+    free(stack);
+    stack = NULL;
   }
 }
